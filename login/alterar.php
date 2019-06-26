6b9c8e6baa04ec2a8824fb8379bf2d73c9563510
xShinohara <julianoKS1999@hotmail.com>
2019-06-25 21:26:11 -0300

    Alisson gostosao

---
+++ login/alterar.php
@@ -0,0 +1,25 @@
+<?php
+ //require_once "config.php";
+ ?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <title>Sorvete In Box</title>
+  <link href="https://fonts.googleapis.com/css?family=Noto+Serif+SC:700|Slabo+27px" rel="stylesheet">
+  <link rel="stylesheet" href="_css/login.css">
+  <div id="interface">
+    <form id="login">
+  		<legend id="Lentrar">Digite sua nova senha:</legend>
+  		<!-- Lemail e Lsenha: O 'L' significa Login -->
+      <p><input type="password" name="password" id="password" size="20" maxlenght="35"></p>
+
+      <legend id="Lentrar">Digite seu novo telefone</legend>
+
+      <p><input type="number" name="tel" id="tel" maxlenght="11"></p>
+
+      <button type="button" id="buton" Onclick="log(Lemail.value, Lsenha.value)">Confirmar</button>
+
+  		<p id="cima"><a href="index.php" id="unic">Voltar</a></p>
+  	</form>
+  </div>
